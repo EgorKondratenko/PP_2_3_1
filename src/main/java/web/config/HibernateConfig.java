f4dbbbf1904a0db88c49b9c42980a51a191ce2e1
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-04-18 00:33:52 +0300

    Add files via upload

--- src/main/java/web/config/HibernateConfig.java
+++ src/main/java/web/config/HibernateConfig.java
@@ -9,12 +9,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -57,10 +55,5 @@ public class HibernateConfig {
         transactionManager.setSessionFactory((SessionFactory) entityManagerFactory().getObject());
         return transactionManager;
     }
-//    @Bean
-//    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(entityManagerFactory);
-//        return transactionManager;
-//    }
+
 }
