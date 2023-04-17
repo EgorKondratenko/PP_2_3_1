0524e2dfa869951824521c458b498d56953dc95c
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-04-18 00:34:17 +0300

    Add files via upload

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,39 +2,58 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
+import java.util.List;
+
 @Controller
 public class UserController {
 
-    private UserService userService;
+private final UserService userService;
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/main")
-    public String viewUser (ModelMap modelMap) {
-        modelMap.addAttribute("listUsers", userService.listUsers());
+    @GetMapping("/main")
+    public String getUsers(Model model){
+        List<User> users = userService.getAll();
+        model.addAttribute("main", users);
         return "main";
     }
+    @GetMapping("/addUser")
+    public String addUser() {
+        return "addUser";
+    }
+
     @PostMapping("/addUser")
-    public String addUser(@ModelAttribute("user") User user) {
-        userService.add(user);
-        return "redirect:/main";
-    }
-    @PatchMapping("/updateUser")
-    public String updateUser(@ModelAttribute("user") User user) {
-        userService.update(user);
-        return "redirect:/main";
-    }
-    @DeleteMapping("/deleteUser")
-    public String deleteUser(@ModelAttribute("id") Long id) {
-        userService.delete(id);
+    public String addUserPost(@ModelAttribute("user") User user){
+        userService.addUser(user);
         return "redirect:/main";
     }
 
+    @GetMapping("/updateUser/{id}")
+    public String updateUser(@PathVariable("id") Long id,Model model){
+        model.addAttribute("user",userService.getUser(id));
+        return "updateUser";
+    }
+
+    @PatchMapping("/updateUser/{id}")
+    public String updateUserPost(@ModelAttribute User user,@PathVariable("id") Long id){
+        userService.updateUser(user,id);
+        return "redirect:/main";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/main";
+    }
 }
+
+
+
